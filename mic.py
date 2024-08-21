247773f08f9ba5c13acaf550913cdc78af4691b2
MHamzaFateh <155734571+MHamzaFateh@users.noreply.github.com>
2024-08-22 00:12:55 +0500

    Update mic.py

--- mic.py
+++ mic.py
@@ -7,8 +7,7 @@ import tempfile
 import speech_recognition as sr
 from datasets import load_dataset
 from deep_translator import GoogleTranslator
-from playsound import playsound
-# import pygame
+import pygame
 
 
 # Retrieve API key from environment variable
@@ -40,27 +39,20 @@ def conversational_retrieval(query, chat_history):
 def translate_text(text, src_lang='en', dest_lang='ur'):
     return translator.translate(text)
 
-# Function to speak text
-# def speak_text(text, lang='ur'):
-#     tts = gTTS(text=text, lang=lang)
-#     audio_file = "response.mp3"
-#     tts.save(audio_file)
-
-#     pygame.mixer.init()
-#     pygame.mixer.music.load(audio_file)
-#     pygame.mixer.music.play()
-
-#     while pygame.mixer.music.get_busy():
-#         pygame.time.Clock().tick(10)
-
-#     pygame.mixer.quit()
-
-
+Function to speak text
 def speak_text(text, lang='ur'):
     tts = gTTS(text=text, lang=lang)
     audio_file = "response.mp3"
     tts.save(audio_file)
-    playsound(audio_file)
+
+    pygame.mixer.init()
+    pygame.mixer.music.load(audio_file)
+    pygame.mixer.music.play()
+
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(10)
+
+    pygame.mixer.quit()
 
 
 # Function to recognize speech from audio
