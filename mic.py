58304dbdfeb2bec13f02b07c250a0452fc61447e
MHamzaFateh <155734571+MHamzaFateh@users.noreply.github.com>
2024-08-21 21:04:59 +0500

    Update mic.py

--- mic.py
+++ mic.py
@@ -1,34 +1,37 @@
-import os
-import numpy as np
 import streamlit as st
-from io import BytesIO
-import streamlit.components.v1 as components
+from bokeh.models.widgets import Button
+from bokeh.models import CustomJS
+from streamlit_bokeh_events import streamlit_bokeh_events
 
-def st_audiorec():
-    parent_dir = os.path.dirname(os.path.abspath(__file__))
-    build_dir = os.path.join(parent_dir, "frontend/build")
-    st_audiorec = components.declare_component("st_audiorec", path=build_dir)
+stt_button = Button(label="Speak", width=100)
 
-    raw_audio_data = st_audiorec()
-    wav_bytes = None
+stt_button.js_on_event("button_click", CustomJS(code="""
+    var recognition = new webkitSpeechRecognition();
+    recognition.continuous = true;
+    recognition.interimResults = true;
+ 
+    recognition.onresult = function (e) {
+        var value = "";
+        for (var i = e.resultIndex; i < e.results.length; ++i) {
+            if (e.results[i].isFinal) {
+                value += e.results[i][0].transcript;
+            }
+        }
+        if ( value != "") {
+            document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
+        }
+    }
+    recognition.start();
+    """))
 
-    if isinstance(raw_audio_data, dict):
-        with st.spinner('Retrieving audio recording...'):
-            ind, raw_audio_data = zip(*raw_audio_data['arr'].items())
-            ind = np.array(ind, dtype=int)
-            raw_audio_data = np.array(raw_audio_data)
-            sorted_ints = raw_audio_data[ind]
-            stream = BytesIO(b"".join([int(v).to_bytes(1, "big") for v in sorted_ints]))
-            wav_bytes = stream.read()
+result = streamlit_bokeh_events(
+    stt_button,
+    events="GET_TEXT",
+    key="listen",
+    refresh_on_update=False,
+    override_height=75,
+    debounce_time=0)
 
-    return wav_bytes
-
-# Main app logic
-st.title("Streamlit Audio Recorder")
-audio_data = st_audiorec()
-
-if audio_data:
-    st.audio(audio_data, format="audio/wav")
-    st.success("Audio recorded successfully!")
-else:
-    st.warning("No audio recorded yet.")
+if result:
+    if "GET_TEXT" in result:
+        st.write(result.get("GET_TEXT"))
